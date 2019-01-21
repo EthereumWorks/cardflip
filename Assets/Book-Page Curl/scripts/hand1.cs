911f5b39442eff292ce49cb6d91eefed3f870502
Vyacheslav Igorevich Poskonin <v.poskonin@gmail.com>
2019-01-21 19:47:59 +0700

    all

---
+++ Assets/Book-Page Curl/scripts/hand1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float distance = 10f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); // переменной записываються координаты мыши по иксу и игрику
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваеться переменная с координатами мыши
+        transform.position = objPosition; // и собственно объекту записываються координаты
+    }
+}
